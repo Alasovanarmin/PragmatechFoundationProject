e7568f53925ef168b27ce60b84a62b90da2f9bac
Alasovanarmin <nerminalesova@gmail.com>
2022-04-07 15:38:53 +0400

    add file

--- pythonstart/tasks.py
+++ pythonstart/tasks.py
@@ -1,1 +1,7 @@
-print(3+'3')
+#This is a comment.
+#print(3+'3') kodunun neticesi error olacaq cunki compiler ferqli tipli deyisenlere aid etdiyi ucun tercume ede bilmeyecek.
+#print(true+1) kodunun neticesi: "true" is not defined cunki T herfi boyuk herfle yazilmalidi.
+#print(3>4 and 3+4/2) kodunun neticesi: "False" olacaq cunki 3 reqemi 4-den boyuk deyil.
+#print(type(int(float('3')))) kodunun neticesi: <class 'int'> cunki 3 tam eded oldugu ucun compiler onu integer tipli deyisenlere ayirir.
+#print(3.14+int(3.14)) kodunun neticesi: "6.140000000000001"
+#print(False+True>int(False)) kodunun neticesi: 'True' cunki  True+False cemi 1-e beraber olacaq ve False(yeni 0)-dan boyuk olacaq.
