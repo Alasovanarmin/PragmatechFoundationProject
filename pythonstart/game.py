15437b0cb074222deda6d54d6336abf4cc8cc88a
Alasovanarmin <nerminalesova@gmail.com>
2022-04-19 21:06:14 +0400

    add file

--- pythonstart/game.py
+++ pythonstart/game.py
@@ -1,1 +1,6 @@
-#print("Hello world!")
+
+#1. Else ve elif arasinda ferq nedir?- Serti konstruksiyasi ucun "if" serti mecburidi. "Basqa Eger " veziyyeti "if" prefiksi olmadan
+# movcud ola bilmez. Hem "if" ve " Else" varsa , kodun budaqlanmasina komek edir. "If" bolmesinde yazilan emr dogru deyilse avtomatik
+# olaraq "else" bolmesindeki emre kecir.
+#2.mən if else istifadə edəndə niyə boşluq istifadə edirəm?
+# Cunki sintax bucurdur.
