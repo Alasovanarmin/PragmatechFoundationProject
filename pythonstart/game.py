f253a73b8c351bf1985b04cb3678560b4971f96c
Alasovanarmin <nerminalesova@gmail.com>
2022-04-24 13:20:51 +0400

    Homework completed

--- pythonstart/game.py
+++ pythonstart/game.py
@@ -1,4 +1,4 @@
-
+#print("Hello world!")
 #1. Else ve elif arasinda ferq nedir?- Serti konstruksiyasi ucun "if" serti mecburidi. "Basqa Eger " veziyyeti "if" prefiksi olmadan
 # movcud ola bilmez. Hem "if" ve " Else" varsa , kodun budaqlanmasina komek edir. "If" bolmesinde yazilan emr dogru deyilse avtomatik
 # olaraq "else" bolmesindeki emre kecir.
---
+++ pythonstart/input.py
@@ -0,0 +1,12 @@
+maas=input('Aldiginiz maasi qeyd edin:')
+print(type(maas))
+print(type(int(maas)))
+netice=int(maas)
+netice02=int(maas)*int(maas)
+if netice:
+    print('Netice: Ededdir')
+    print(netice02)
+
+    
+
+ 
